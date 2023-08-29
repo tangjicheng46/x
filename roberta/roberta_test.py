b1f08da19888df8df3b560112801a3ed4c7529b5
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-29 15:40:15 +0900

    chore: init roberta

---
+++ roberta/roberta_test.py
@@ -0,0 +1,46 @@
+import time
+import transformers
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+cache_dir = "./temp"
+
+tokenizer = AutoTokenizer.from_pretrained("nlp-waseda/roberta-base-japanese", cache_dir=cache_dir)
+model = AutoModelForMaskedLM.from_pretrained("nlp-waseda/roberta-base-japanese", cache_dir=cache_dir)
+
+def embedding_func(text: str):
+    encoding = tokenizer(text, return_tensors='pt')
+    output = model(**encoding)
+    return output
+
+def benchmark(N, input_list, input_func, warmup=10):
+    for _ in range(warmup):
+        for item in input_list:
+            print(item)
+            result = input_func(item)
+
+    start_time = time.time()
+
+    for _ in range(N):
+        for item in input_list:
+            result = input_func(item)
+
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    average_time_per_element = elapsed_time / (N * len(input_list))
+
+    print(f"Average time per input: {average_time_per_element:.6f} seconds")
+
+def read_lines_to_list(filename):
+    lines_list = []
+    with open(filename, 'r') as file:
+        for line in file:
+            lines_list.append(line.strip())
+    return lines_list
+
+if __name__ == "__main__":
+    input_text = "./input_text.txt"
+    input_list = read_lines_to_list(input_text)
+
+    benchmark(100, input_list, embedding_func)
+
