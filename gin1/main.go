c8ea0b7dc955b7e6e4d5d5924df86ab45d706b78
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-09-26 11:44:49 +0900

    chore: update

---
+++ gin1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 创建一个Gin引擎
+	r := gin.New()
+
+	// 设置Gin模式为发布模式
+	gin.SetMode(gin.ReleaseMode)
+
+	// 定义一个路由，对 "/hello" 返回 "hello, world!" 并返回 200 OK
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(200, "hello, world!")
+	})
+
+	// 启动Gin服务器，默认监听在 ":8080" 端口
+	r.Run()
+}
