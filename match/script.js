550a3030f3b1760819d2ebc443ecf5b878c0c8db
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-09-27 12:45:06 +0900

    chore: update

--- match/script.js
+++ match/script.js
@@ -6,7 +6,6 @@ const fruitMap = {
     '柠檬': 'lemon'
 };
 
-let selectedWords = [];
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -14,6 +13,8 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]]; // 交换
     }
 }
+
+const totalWords = 4;
 
 function getRandomSubmap(map, n) {
     const keys = Object.keys(map);
@@ -30,7 +31,7 @@ function getRandomSubmap(map, n) {
 
 
 function generateWords() {
-    newMap = getRandomSubmap(fruitMap, 4)
+    newMap = getRandomSubmap(fruitMap, totalWords)
     const chineseWords = Object.keys(newMap);
     const englishWords = Object.values(newMap);
 
@@ -47,7 +48,7 @@ function generateWords() {
         const button = document.createElement('button');
         button.textContent = word;
         button.className = 'button';
-        button.onclick = () => wordClicked(word, 'chinese');
+        button.onclick = () => wordClicked(word, 'chinese', button); // 将按钮元素也传递给wordClicked函数
         chineseContainer.appendChild(button);
     });
 
@@ -55,28 +56,57 @@ function generateWords() {
         const button = document.createElement('button');
         button.textContent = word;
         button.className = 'button';
-        button.onclick = () => wordClicked(word, 'english');
+        button.onclick = () => wordClicked(word, 'english', button); // 将按钮元素也传递给wordClicked函数
         englishContainer.appendChild(button);
     });
 }
 
+let selectedWords = [];
+let matchCount = 0;
 function checkMatch() {
-    const [first, second] = selectedWords;
-    if (first.language === second.language) {
+    const selected = [...selectedWords]; // 保存对selectedWords的引用
+    selectedWords = []; // 立即重置selectedWords数组
+    
+    const [firstButton, secondButton] = selected;
+    if (firstButton.language === secondButton.language) {
         alert('请选择不同语言的单词！');
+        resetButtonColor(firstButton);
+        resetButtonColor(secondButton);
     } else {
-        const chineseWord = first.language === 'chinese' ? first.word : second.word;
-        if (fruitMap[chineseWord] === (first.language === 'english' ? first.word : second.word)) {
-            alert('匹配成功！');
-            generateWords();
+        const chineseWord = firstButton.language === 'chinese' ? firstButton.word : secondButton.word;
+        if (fruitMap[chineseWord] === (firstButton.language === 'english' ? firstButton.word : secondButton.word)) {
+            matchCount++;
+            firstButton.element.style.backgroundColor = 'green';
+            secondButton.element.style.backgroundColor = 'green';
+            
+            if (matchCount === totalWords) {
+                alert('全部匹配成功！');
+                matchCount = 0;
+                generateWords();
+            }
         } else {
             alert('匹配失败，请重试！');
+            resetButtonColor(firstButton);
+            resetButtonColor(secondButton);
         }
     }
 }
 
-function wordClicked(word, language) {
-    selectedWords.push({ word, language });
+function resetButtonColor(button) {
+    // 只有当按钮颜色不是绿色时才重置颜色
+    if (button.element.style.backgroundColor !== 'green') {
+        button.element.style.backgroundColor = '';
+    }
+}
+
+function wordClicked(word, language, element) {
+    // 如果按钮颜色已经是绿色，则不允许用户选择
+    if (element.style.backgroundColor === 'green') {
+        return;
+    }
+    
+    selectedWords.push({ word, language, element }); // 将元素也传递到对象中
+    element.style.backgroundColor = 'yellow'; // 当单词被选中时，改变按钮颜色为黄色
     if (selectedWords.length === 2) {
         checkMatch();
         selectedWords = [];
